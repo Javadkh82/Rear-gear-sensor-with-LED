04628e7e15366cf79d17371a6d57c5a833f7dcec
javad_KH <166110182+Javadkh82@users.noreply.github.com>
2024-04-25 08:29:54 +0330

    Rear gear sensor with LED .ino

---
+++ Rear gear sensor with LED .ino
@@ -0,0 +1,47 @@
+/*
+     Ultrasonic Sensor HC-SR05/4 and Arduino
+      by Hanie Kiani
+   https://electropeak.com/learn
+*/
+long duration;
+int distance;
+const int trig = 9;
+const int echo = 10;
+int led = 3;
+
+
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int distance = calculateDistance();
+
+  if (distance < 10) {
+    digitalWrite(led, HIGH);
+  }
+  else if (distance > 30) {
+    digitalWrite(led, LOW);
+  }
+  else {
+    int range = map(distance, 10, 30, 255, 0);
+    analogWrite(led, range);
+  }
+
+    Serial.print ("distance : ");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+
+  int calculateDistance() {
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    duration = pulseIn(echo, HIGH);
+    distance = (duration * 0.034 / 2);
+    return distance;
+  }
